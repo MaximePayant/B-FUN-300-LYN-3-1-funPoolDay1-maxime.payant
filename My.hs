46482dfd07c42bbd2c4ca6b02d03002653e66f3d
Maxime Payant <maxime@pop-os.localdomain>
2021-01-26 14:02:42 +0100

    ex02 and norm conditional

--- My.hs
+++ My.hs
@@ -9,28 +9,20 @@ mySucc :: Int -> Int
 mySucc x = x + 1
 
 myIsNeg :: Int -> Bool
-myIsNeg x =
-    if x < 0
-        then False
-        else True
+myIsNeg x | x < 0 = True
+          | otherwise = False
 
 myAbs :: Int -> Int
-myAbs x =
-    if x < 0
-        then (-x)
-        else x
+myAbs x | x < 0 = (-x)
+        | otherwise = x
 
 myMin :: Int -> Int -> Int
-myMin x y =
-    if x < y
-        then x
-        else y
+myMin x y | x < y = x
+          | otherwise = y
 
 myMax :: Int -> Int -> Int
-myMax x y =
-    if x > y
-        then x
-        else y
+myMax x y | x > y = x
+          | otherwise = y
 
 myTuple :: a -> b -> (a, b)
 myTuple a b = (a, b)
@@ -39,22 +31,22 @@ myTruple :: a -> b -> c -> (a, b, c)
 myTruple a b c = (a, b, c)
 
 myFst :: (a, b) -> a
-myFst (a, b) = a
+myFst (a, _) = a
 
 mySnd :: (a, b) -> b
-mySnd (a, b) = b
+mySnd (_, b) = b
 
 mySwap :: (a, b) -> (b, a)
 mySwap (a, b) = (b, a)
 
 myHead :: [a] -> a
 myHead [] = error "Empty list"
-myHead (x:xs) = x
+myHead (x:_) = x
 
 myTail :: [a] -> [a]
 myTail [] = error "Empty list"
-myTail (x:xs) = xs
+myTail (_:xs) = xs
 
 myLength :: [a] -> Int
 myLength [] = 0
-myLength (x:xs) = 1 + myLength xs
+myLength (_:xs) = 1 + myLength xs
