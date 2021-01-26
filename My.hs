14818f307d1948737a58b5e7d92d70f9d2b20265
Maxime Payant <maxime@pop-os.localdomain>
2021-01-26 11:29:30 +0100

    ex06 to ex13

--- My.hs
+++ My.hs
@@ -5,10 +5,10 @@
 --My.hs
 -}
 
-mySucc :: Integer -> Integer
+mySucc :: Int -> Int
 mySucc x = x + 1
 
-myIsNeg :: Integer -> Bool
+myIsNeg :: Int -> Bool
 myIsNeg x =
     if x < 0
         then False
@@ -31,3 +31,30 @@ myMax x y =
     if x > y
         then x
         else y
+
+myTuple :: a -> b -> (a, b)
+myTuple a b = (a, b)
+
+myTruple :: a -> b -> c -> (a, b, c)
+myTruple a b c = (a, b, c)
+
+myFst :: (a, b) -> a
+myFst (a, b) = a
+
+mySnd :: (a, b) -> b
+mySnd (a, b) = b
+
+mySwap :: (a, b) -> (b, a)
+mySwap (a, b) = (b, a)
+
+myHead :: [a] -> a
+myHead [] = error "Empty list"
+myHead (x:xs) = x
+
+myTail :: [a] -> [a]
+myTail [] = error "Empty list"
+myTail (x:xs) = xs
+
+myLength :: [a] -> Int
+myLength [] = 0
+myLength (x:xs) = 1 + myLength xs
